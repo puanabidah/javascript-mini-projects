d3b304e5d885a10f7a30c6ffda7ddc1184dee98f
Puan Abidah Nitisara <puanabidah13@gmail.com>
2025-10-03 10:25:10 +0700

    switch statement

--- js-keywords-exercises/index.js
+++ js-keywords-exercises/index.js
@@ -41,3 +41,54 @@ let discount = purchaseAmount > 100 ? 10 : 0;
 console.log(
   `Your total is $${purchaseAmount - purchaseAmount * (discount / 100)}`
 );
+
+// switch statement can be effecient replacement for multiple if else if statements
+let day = 7;
+
+switch (day) {
+  case 1:
+    console.log('It is Monday');
+    break;
+  case 2:
+    console.log('It is Tuesday');
+    break;
+  case 3:
+    console.log('It is Wednesday');
+    break;
+  case 4:
+    console.log('It is Thursday');
+    break;
+  case 5:
+    console.log('It is Friday');
+    break;
+  case 6:
+    console.log('It is Saturday');
+    break;
+  case 7:
+    console.log('It is Sunday');
+    break;
+  default:
+    console.log('Invalid day');
+}
+
+let testScore = 55;
+let letterGrade;
+
+switch (true) {
+  case testScore >= 90:
+    letterGrade = 'A';
+    break;
+  case testScore >= 80:
+    letterGrade = 'B';
+    break;
+  case testScore >= 70:
+    letterGrade = 'C';
+    break;
+  case testScore >= 60:
+    letterGrade = 'D';
+    break;
+  default:
+    letterGrade = 'F';
+}
+
+console.log(`Your letter grade is: ${letterGrade}`);
