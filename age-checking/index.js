cf4ab4280634a909f539a52852144ecd33ab3495
Puan Abidah Nitisara <puanabidah13@gmail.com>
2025-10-12 22:23:59 +0700

    modify interface and clean the code

--- age-checking/index.js
+++ age-checking/index.js
@@ -6,6 +6,7 @@ let age;
 submitBtn.onclick = function () {
   const input = myText.value.trim();
   age = Number(input);
+  result.style.display = 'block';
 
   if (input === '') {
     result.textContent = 'You must enter a number.';
@@ -18,13 +19,13 @@ submitBtn.onclick = function () {
   } else if (age < 0) {
     result.textContent = `Your age can't be below 0.`;
   } else if (age === 0) {
-    result.textContent = `Your Age is ${age}.\nYou can't enter. You were just born.`;
+    result.textContent = `Your age is ${age} years old.\nYou can't enter. You were just born.`;
   } else if (age >= 100) {
-    result.textContent = `Your Age is ${age}.\nYou are TOO OLD to enter this site.`;
+    result.textContent = `Your age is ${age} years old.\nYou are TOO OLD to enter this site.`;
   } else if (age >= 18) {
-    result.textContent = `Your Age is ${age}.\nYou are old enough to enter this site.`;
+    result.textContent = `Your age is ${age} years old.\nYou are old enough to enter this site.`;
   } else {
-    result.textContent = `Your Age is ${age}.\nYou must be 18+ to enter this site.`;
+    result.textContent = `Your age is ${age} years old.\nYou must be 18+ to enter this site.`;
   }
 
   myText.value = '';
