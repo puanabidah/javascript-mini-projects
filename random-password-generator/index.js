cbf52daaf34aa8b77ba2b9b5cab0df744a67fd95
Puan Abidah Nitisara <puanabidah13@gmail.com>
2025-10-16 16:11:33 +0700

    make a random password generator

---
+++ random-password-generator/index.js
@@ -0,0 +1,48 @@
+function generatePassword(
+  length,
+  includeLowerCase,
+  includeUpperCase,
+  includeNumbers,
+  includeSymbols
+) {
+  const lowerCaseChars = 'abcdefghijklmnopqrstuvwxyz';
+  const upperCaseChars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+  const numberChars = '0123456789';
+  const symbolChars = '!@#$%^&*()_+-=';
+  let allowedChars = '';
+  let password = '';
+
+  allowedChars += includeLowerCase ? lowerCaseChars : '';
+  allowedChars += includeUpperCase ? upperCaseChars : '';
+  allowedChars += includeNumbers ? numberChars : '';
+  allowedChars += includeSymbols ? symbolChars : '';
+
+  if (length <= 0) {
+    return `(Password length must be at least 1)`;
+  }
+  if (allowedChars.length === 0) {
+    return `(At least 1 set of character needs to be selected)`;
+  }
+
+  for (let i = 0; i < length; i++) {
+    // Generate a random index within the bounds of your character set string
+    const randomIndex = Math.floor(Math.random() * allowedChars.length);
+    password += allowedChars[randomIndex];
+  }
+  return password;
+}
+const passwordLength = 10;
+const includeLowerCase = true;
+const includeUpperCase = true;
+const includeNumbers = true;
+const includeSymbols = true;
+
+const password = generatePassword(
+  passwordLength,
+  includeLowerCase,
+  includeUpperCase,
+  includeNumbers,
+  includeSymbols
+);
+
+console.log(`Generated password: ${password}`);
